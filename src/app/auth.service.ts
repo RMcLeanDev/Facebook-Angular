a1cc1df10e39009c1634186679401324c47977fb
Ryan McLean <mcleanrs@comcast.net>
2019-04-05 08:40:24 -0700

    add proper imports for authentication

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { User } from './model/login.model';
+import { AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
 
 @Injectable()
+
 export class AuthService {
 
   constructor() { }
