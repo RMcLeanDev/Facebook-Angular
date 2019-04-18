4f8799942fba79633c3abe3234275af6dbf60f99
Student <example@example.com>
2019-04-18 13:25:53 -0700

    adjust time of build in console log & add rules to fix issue with refresh

--- src/main.ts
+++ src/main.ts
@@ -11,4 +11,4 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
 
-console.log("Time of Build 4/6/19 @ 4:24PMM")
+console.log("Time of Build 4/18/19 @ 1:25PM")
