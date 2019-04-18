95bb0a2d301b3a685d0a83dbfa6b96f6169a1e7a
Student <example@example.com>
2019-04-18 15:27:25 -0700

    add custom hover on all icons including arrows n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component, Pipe, Input } from '@angular/core';
+import { Component, Pipe, Input, OnInit } from '@angular/core';
 import { User, RecentLogin } from './model/login.model';
 import { DomSanitizer } from '@angular/platform-browser';
+import { AuthService } from './auth.service';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [AuthService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(public authService: AuthService) {}
+  ngOnInit() {
+    this.authService.user.subscribe(user => {
+      if (user == null){
+      } else {
+        this.disclaimer = false;
+      }
+    });
+  };
   disclaimer = true;
   hideDisclaimer(){
     this.disclaimer=false;
