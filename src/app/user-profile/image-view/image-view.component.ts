33de4d60c53a1e91e6d2f2f541f216a9ef936045
Student <example@example.com>
2019-04-29 16:14:13 -0700

    restyle image-view to be more like facebook. add side panel where comments will go

--- src/app/user-profile/image-view/image-view.component.ts
+++ src/app/user-profile/image-view/image-view.component.ts
@@ -1,19 +1,33 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AppService } from '../../app.service';
+import * as firebase from 'firebase';
+import { AuthService } from '../../auth.service';
+import { ActivatedRoute, Params} from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-image-view',
   templateUrl: './image-view.component.html',
   styleUrls: ['./image-view.component.scss'],
-  providers: [AppService]
+  providers: [AppService, AuthService]
 })
 export class ImageViewComponent implements OnInit {
   @Input() masterSelectedImage;
   @Output() closeImage = new EventEmitter;
-  constructor(private appService: AppService) {}
+  constructor(private appService: AppService, private authService: AuthService, private route: ActivatedRoute) {}
   imageViewer = false;
+  userId;
+  user;
+  activeUserId;
+  activeUser;
   ngOnInit() {
-    console.log(this.masterSelectedImage.$key)
+    this.route.params.forEach((urlParamaters) => {
+      this.userId = urlParamaters['id'];
+    });
+    this.user = this.appService.getUserById(this.userId)
+    let activeUserId = firebase.auth().currentUser;
+    this.activeUserId = firebase.auth().currentUser.uid;
+    this.activeUser = this.appService.getUserById(this.activeUserId);
   }
 
   viewImage(){
